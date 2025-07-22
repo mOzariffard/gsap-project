c115a05135f9103c60bcdd5357c546095de85794
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-07-22 14:48:46 +0300

    feat(about section):add everything in about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import NavBar from '@/components/NavBar';
 import HeroSection from '@/components/HeroSection';
-import CocktailSection from './components/CocktailSection';
+import CocktailSection from '@/components/CocktailSection';
+import AboutSection from '@/components/AboutSection';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ function App() {
      <NavBar/>
      <HeroSection/>
      <CocktailSection/>
+     <AboutSection/>
     </main>
   )
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import ab1 from "/images/abt1.png";
+import ab2 from "/images/abt2.png";
+import ab3 from "/images/abt3.png";
+import ab4 from "/images/abt4.png";
+import ab5 from "/images/abt5.png";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { SplitText } from 'gsap/all';
+
+const AboutSection = () => {
+    useGSAP(()=>{
+        const titleSplit = SplitText.create('#about h2', {type: 'words'});
+
+        const scrollTimeline= gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center',
+            }
+        })
+
+        scrollTimeline.from(titleSplit.words,{
+            opacity:0 , duration:1 , yPercent:100 , ease:'expo.inOut', stagger: 0.02
+        })
+        .from('.top-grid div , .bottom-grid div',{
+            opacity:0 , duration: 1 , ease:'power1.inOut', stagger:0.04}, '-=0.5')
+    },[]);
+
+  return (
+    <section id='about'>
+        <div className='mb-16 md:px-0 px-5'>
+            <div className='content'>
+                <div className='md:col-span-8 '>
+                    {/* ---- HEAD OF ABOUT US ---- */}
+                    <p className='badge'>Best Cocktails</p>
+                    <h2> 
+                    Where every details matters <span className='text-white font-semibold'>-</span>
+                    from muddle to garnish 
+                    </h2>
+                </div>
+
+                {/*  */}
+                <div className='sub-content'>
+                    <p>
+                        Every Cocktail we serve is reflection of our passion for mixology.
+                        Our team of skilled bartenders meticulously crafts each drink, 
+                        ensuring that every ingredient is perfectly balanced to create a harmonious flavor profile. 
+                    </p>
+
+                    <div>
+                        <p className='md:text-3xl text-xl font-bold s'>
+                            <span>4.5</span>/5
+                        </p>
+                        <p className='text-sm text-white-100'>
+                            More than 1000+ reviews from our happy customers
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='top-grid'>
+            <div className='md:col-span-3 '>
+                <div className='noisy'/>
+                    <img src={ab1} alt="ab1" />
+            </div>
+
+
+            <div className='md:col-span-6 '>
+                <div className='noisy'/>
+                    <img src={ab2} alt="ab2" />
+            </div>
+
+
+            <div className='md:col-span-3 '>
+                <div className='noisy'/>
+                    <img src={ab5} alt="ab5" />
+            </div>
+        </div>
+
+        <div className='bottom-grid'>
+            <div className='md:col-span-8'> 
+                <div className='noisy' />
+                <img src={ab3} alt="ab3" />
+            </div>
+
+
+            <div className='md:col-span-4'> 
+                <div className='noisy' />
+                <img src={ab4} alt="ab4" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default AboutSection
